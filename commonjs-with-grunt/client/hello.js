32a6c986a5ecef42e22fa0d50527f72a75dc0b3b
prabirshrestha <mail@prabir.me>
2013-12-21 20:42:06 -0500

    fixed spacing

--- commonjs-with-grunt/client/hello.js
+++ commonjs-with-grunt/client/hello.js
@@ -2,11 +2,11 @@
 var React = require('react');
 
 var Hello = React.createClass({
-  render: function() {
-    return (
-        <h1>Hello World</h1>
-    );
-  }
+    render: function() {
+        return (
+            <h1>Hello World</h1>
+        );
+    }
 });
 
 module.exports = Hello;
